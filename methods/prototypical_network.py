1ea3134cdce5794faeef02d8ad71b996376e5a03
victor-nasc <victor_nascimento@usp.br>
2025-01-22 16:02:56 -0300

    add Protonets

---
+++ methods/prototypical_network.py
@@ -0,0 +1,45 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class PrototypicalNetworks(nn.Module):
+    '''
+    Implementacao de um notebook de exemplo do easyfsl
+    a classe que esta na lib e pode ser importada está desatualizada ;-;
+    '''
+    def __init__(self, backbone: nn.Module):
+        super(PrototypicalNetworks, self).__init__()
+        self.backbone = backbone
+
+    def forward(
+        self,
+        support_images: torch.Tensor,
+        support_labels: torch.Tensor,
+        query_images: torch.Tensor,
+    ) -> torch.Tensor:
+        """
+        Predict query labels using labeled support images.
+        """
+        # Extract the features of support and query images
+        z_support = self.backbone.forward(support_images)
+        z_query = self.backbone.forward(query_images)
+
+        # Infer the number of different classes from the labels of the support set
+        n_way = len(torch.unique(support_labels))
+        # Prototype i is the mean of all instances of features corresponding to labels == i
+        z_proto = torch.cat(
+            [
+                z_support[torch.nonzero(support_labels == label)].mean(0)
+                for label in range(n_way)
+            ]
+        )
+
+        # Compute the euclidean distance from queries to prototypes
+        dists = torch.cdist(z_query, z_proto)
+
+        # And here is the super complicated operation to transform those distances into classification scores!
+        scores = -dists
+        
+        # adicionando softmax na saida
+        return F.softmax(scores, dim=1)
