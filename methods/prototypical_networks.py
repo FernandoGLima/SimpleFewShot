ac420bf4a5d4d464d030270141b69dbc8562274d
victor-nasc <victor_nascimento@usp.br>
2025-04-16 16:39:07 -0300

    add references and update vars

--- methods/prototypical_networks.py
+++ methods/prototypical_networks.py
@@ -2,12 +2,11 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
+# Prototypical Networks for Few-shot Learning
+# https://arxiv.org/abs/1703.05175
+# based on the open-source implementation of https://github.com/sicara/easy-few-shot-learning
 
 class PrototypicalNetworks(nn.Module):
-    '''
-    Implementacao de um notebook de exemplo do easyfsl: https://github.com/sicara/easy-few-shot-learning
-    a classe que esta na lib e pode ser importada está desatualizada ;-;
-    '''
     def __init__(self, backbone: nn.Module):
         super(PrototypicalNetworks, self).__init__()
         self.backbone = backbone
@@ -17,26 +16,22 @@ class PrototypicalNetworks(nn.Module):
         support_images: torch.Tensor,
         support_labels: torch.Tensor,
         query_images: torch.Tensor,
+        *args, 
     ) -> torch.Tensor:       
 
         # computa features 
         z_support = self.backbone.forward(support_images)
         z_query = self.backbone.forward(query_images)
 
-        # um prototype eh a media de todas as features de cada label
-        support_labels = support_labels.argmax(1) # remove one-hot encoding
-        n_way = len(torch.unique(support_labels))
-        z_proto = torch.cat(
-            [
+        ways = support_labels.shape[1] 
+
+        # compute prototype
+        support_labels = support_labels.argmax(1) 
+        z_proto = torch.cat([
                 z_support[torch.nonzero(support_labels == label)].mean(0)
-                for label in range(n_way)
-            ]
-        ) # shape: [n_way, feature_size]
+                for label in range(ways)
+            ]) # shape: [ways, hdim]
 
-        # distancia euclidiana
-        dists = torch.cdist(z_query, z_proto) 
+        scores = -torch.cdist(z_query, z_proto) 
 
-        # scores sao as distancias negativas (quanto menor a distancia, maior o score)
-        scores = -dists
-        
         return F.softmax(scores, dim=1)
