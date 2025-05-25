164b6f1ffbfaa5df8a23642c599bab16e1541401
Fernando Gouveia Lima <fernandolimabusiness@gmail.com>
2025-05-25 09:51:22 -0300

    New models

---
+++ simplefsl/models/DSN.py
@@ -0,0 +1,63 @@
+import torch
+from torch.autograd import Variable
+import torch.nn as nn
+import torch.nn.functional as F
+
+# DSN: Adaptive Subspaces for Few-Shot Learning
+# https://openaccess.thecvf.com/content_CVPR_2020/papers/Simon_Adaptive_Subspaces_for_Few-Shot_Learning_CVPR_2020_paper.pdf
+# Based on the open-source implementation of https://github.com/chrysts/dsn_fewshot/tree/master
+
+class DSN(nn.Module):
+    def __init__(self, backbone):
+        super(DSN, self).__init__()
+        self.backbone = backbone
+
+    def forward(self, train_imgs, train_labels, query_imgs, normalize=False):
+        """
+        Constructs the subspace representation of each class(=mean of support vectors of each class) and
+        returns the classification score (=L2 distance to each class prototype) on the query set.
+
+            Our algorithm using subspaces here
+
+        Parameters:
+            query:  a (tasks_per_batch, n_query, d) Tensor.
+            support:  a (tasks_per_batch, n_support, d) Tensor.
+            support_labels: a (tasks_per_batch, n_support) Tensor.
+            n_way: a scalar. Represents the number of classes in a few-shot classification task.
+            n_shot: a scalar. Represents the number of support examples given per class.
+            normalize: a boolean. Represents whether if we want to normalize the distances by the embedding dimension.
+        Returns: a (tasks_per_batch, n_query, n_way) Tensor.
+        """
+        self.n_way = train_labels.size(1)
+        self.n_shot = train_labels.size(0) // self.n_way
+        
+        support_features = self.backbone.forward(train_imgs)
+        query_features = self.backbone.forward(query_imgs)
+
+        d = query_imgs.size(1)
+        
+        class_representatives = []
+        for nn in range(self.n_way):
+            idxss = train_labels[:, nn].bool()
+            class_support = support_features[idxss]
+            class_support_t = class_support.transpose(0, 1) 
+            class_representatives.append(class_support_t)
+
+        dist = []
+        for class_support_t in class_representatives:
+            uu, _, _ = torch.svd(class_support_t.double())
+            uu = uu.float()
+            subspace = uu[:, :self.n_shot-1].transpose(0, 1)
+            projection = subspace.transpose(0, 1).mm(subspace.mm(query_features.transpose(0, 1))).transpose(0, 1)
+            dist_perclass = torch.sum((query_features - projection)**2, dim=-1)
+            dist.append(dist_perclass)
+
+        dist = torch.stack(dist, dim=1)
+        logits = -dist
+
+        if normalize:
+            logits = logits / d
+
+        logits = F.log_softmax(logits.reshape(-1, self.n_way), dim=1)
+
+        return logits
---
+++ simplefsl/models/MetaQDA.py
@@ -0,0 +1,153 @@
+import torch
+import numpy as np
+import torch.nn as nn
+
+torch.set_default_tensor_type(torch.FloatTensor)
+DEVICE = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+class MetaQDA_MAP(nn.Module):
+    def __init__(self, x_dim, reg_param):
+        super(MetaQDA_MAP, self).__init__()
+        self.x_dim = x_dim
+        self.reg_param = reg_param
+        self.m = torch.nn.Parameter(torch.zeros(1, self.x_dim))
+        self.kappa = torch.nn.Parameter(torch.tensor(0.1))
+        self.nu = torch.nn.Parameter(torch.tensor(self.x_dim, dtype=torch.float32))
+        self.triu_diag = torch.nn.Parameter(torch.ones(self.x_dim))
+        self.triu_lower = torch.nn.Parameter(torch.eye(self.x_dim))
+        self.triu_mask = torch.triu(torch.ones(self.x_dim, self.x_dim), diagonal=1).t().to(DEVICE)
+    
+    def fit_image_label(self, X, y):
+        y_not_one_hot = torch.argmax(y, dim=1)
+        self.classes = np.unique(y_not_one_hot.cpu())
+        self.mu = [None for i in self.classes]
+        self.sigma_inv = [None for i in self.classes]
+        self.lower_triu = torch.diag(torch.abs(self.triu_diag)) + self.triu_lower * self.triu_mask
+        
+        kappa_ = torch.abs(self.kappa) + 1e-6
+        nu_ = torch.clamp(self.nu, min=self.x_dim-1+1e-6)
+        y = torch.argmax(y, dim=1)
+        for j in self.classes:
+            j = int(j)
+            X_j = X[y==j]
+            N_j = X_j.shape[0]
+            self.mu[j] = kappa_.div(kappa_ + N_j)*self.m+torch.div(N_j, kappa_ + N_j)*torch.mean(X_j, dim=0, keepdim=True)
+            sigma_part = self_outer(self.lower_triu) + sum_outer(X_j) + kappa_ * self_outer(self.m)-(kappa_ + N_j) * self_outer(self.mu[j])
+            sigma_j = torch.div(sigma_part, nu_ + N_j + self.x_dim + 2)
+            self.sigma_inv[j] = self.regularize(torch.inverse(sigma_j))          
+
+    def predict(self, X):
+        predicts_matrix=[]      
+        for i in range(X.shape[0]):
+            neg_distrances=[]
+            for mu, sigma_inv in zip(self.mu, self.sigma_inv):
+                diff = X[i, :] - mu
+                gaussian_dist = torch.mm(torch.mm(diff, sigma_inv), diff.t())
+                neg_distrances.append(-1*gaussian_dist)
+
+            predicts_matrix.append(torch.cat(neg_distrances, dim=1))
+        predicts_matrix = torch.cat(predicts_matrix, dim=0)
+        return predicts_matrix
+    
+    def regularize(self, sigma):
+        return (1-self.reg_param) * sigma + self.reg_param * torch.eye(self.x_dim).to(DEVICE)
+
+    
+class MetaQDA_FB(nn.Module):
+    def __init__(self, x_dim, reg_param):
+        super(MetaQDA_FB, self).__init__()
+        self.reg_param = reg_param
+        self.feature_dim = x_dim
+        self.m = torch.nn.Parameter(torch.zeros(1, self.feature_dim))
+        self.kappa = torch.nn.Parameter(torch.tensor(0.1))
+        self.nu = torch.nn.Parameter(torch.tensor(self.feature_dim, dtype=torch.float32))
+        self.triu_diag = torch.nn.Parameter(torch.ones(self.feature_dim))
+        self.triu_lower = torch.nn.Parameter(torch.eye(self.feature_dim))
+        self.triu_lower_mask = torch.triu(torch.ones(self.feature_dim, self.feature_dim), diagonal=1).t().to(DEVICE)
+        
+    def fit_image_label(self, X, y):
+        y_not_one_hot = torch.argmax(y, dim=1)
+        self.classes = np.unique(y_not_one_hot.cpu())
+        self.mu = [None for i in self.classes]
+        self.sigma_inv = [None for i in self.classes]
+        self.biases = [None for i in self.classes]
+        self.common_part = [None for i in self.classes]
+        self.lower_triu = torch.diag(torch.abs(self.triu_diag)) + self.triu_lower * self.triu_lower_mask
+        
+        kappa_ = torch.abs(self.kappa) + 1e-6
+        nu_ = torch.clamp(self.nu, min=self.feature_dim-1+1e-6)
+        y = torch.argmax(y, dim=1)
+        for j in self.classes:
+            j = int(j)
+            X_j = X[y==j]
+            N_j = X_j.shape[0]
+            self.mu[j] = kappa_.div(kappa_ + N_j)*self.m+torch.div(N_j, kappa_ + N_j)*torch.mean(X_j, dim=0, keepdim=True)
+            sigma_j_no_scale = self_outer(self.lower_triu) + sum_outer(X_j) + kappa_ * self_outer(self.m)-(kappa_ + N_j) * self_outer(self.mu[j])
+            sigma_j = torch.div(sigma_j_no_scale, (nu_ + N_j - self.feature_dim + 1)*(kappa_+N_j))*(kappa_+N_j+1)
+            self.sigma_inv[j] = self.regularize(torch.inverse(sigma_j))          
+            self.common_part[j] = nu_ + N_j + 1 - self.feature_dim
+            self.biases[j] = torch.lgamma(0.5*(self.common_part[j]+self.feature_dim))-torch.lgamma(0.5*self.common_part[j])-0.5* self.feature_dim *torch.log(self.common_part[j])-0.5*torch.logdet(sigma_j)
+           
+    def predict(self, X):
+        predicts_matrix=[]      
+        for i in range(X.shape[0]):
+            neg_distrances=[]
+            for mu, sigma_inv, bias, common_part in zip(self.mu, self.sigma_inv, self.biases, self.common_part):
+                neg_distrances.append(bias-0.5*(common_part+self.feature_dim)*torch.log(1.0+(1.0/common_part)*self.compute_distance(X[i, :],mu,sigma_inv)))
+            predicts_matrix.append(torch.cat(neg_distrances, dim=1))
+        predicts_matrix = torch.cat(predicts_matrix, dim=0)
+        return predicts_matrix
+    
+    def compute_distance(self, x, mu, sigma_inv):
+        diff = x - mu
+        gaussian_dist = torch.mm(torch.mm(diff, sigma_inv), diff.t())
+        return gaussian_dist
+    
+    def regularize(self, sigma):
+        return (1-self.reg_param) * sigma + self.reg_param * torch.eye(self.feature_dim).to(DEVICE)   
+    
+### common basic function
+def self_outer(x):
+    dim1, dim2 = x.shape
+    if dim1 >= dim2:
+        return torch.mm(x, x.t())
+    elif dim1 < dim2:
+        return torch.mm(x.t(), x)
+    
+def mean_outer(x):
+    feature_dim = x.shape[1]
+    S_ = torch.zeros((feature_dim,feature_dim), dtype=torch.float32).cuda() 
+    for index, a_v in enumerate(x):
+        S_ += torch.mm(a_v.unsqueeze(1), a_v.unsqueeze(0))
+    
+    return S_.div(index+1)
+
+def sum_outer(x):
+    feature_dim = x.shape[1]
+    S_ = torch.zeros((feature_dim,feature_dim), dtype=torch.float32).cuda() 
+    for a_v in x:
+        S_ += torch.mm(a_v.unsqueeze(1), a_v.unsqueeze(0))
+    
+    return S_
+
+
+# A wrapper around MetaQDA implementations that exposes methods more in line with our codebase.
+class MetaQDA(nn.Module):
+  def __init__(self, backbone, x_dim=2048, variant='FB', reg_param=0.5):
+    super(MetaQDA, self).__init__()
+    self.variant = variant
+    if self.variant == 'FB':
+      self.meta_qda = MetaQDA_FB(x_dim, reg_param)
+    elif self.variant == 'MAP':
+      self.meta_qda = MetaQDA_MAP(x_dim, reg_param)
+
+    self.backbone = backbone
+    self.fe_dim = x_dim
+
+  def forward(self,  train_imgs, train_labels, query_imgs):
+    support_features = self.backbone.forward(train_imgs)
+    query_features = self.backbone.forward(query_imgs)
+
+    self.meta_qda.fit_image_label(support_features, train_labels)
+    prediction = self.meta_qda.predict(query_features)
+    return prediction
