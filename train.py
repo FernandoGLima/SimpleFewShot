b291ea7ff2381bc108139293f83d10f3a29f27a9
carlosmarquesr <carlos.albmr@usp.br>
2025-05-29 21:20:07 -0300

    Added support for swin as backbone

--- train.py
+++ train.py
@@ -6,12 +6,17 @@ from simplefsl.data.manager import FewShotManager
 from simplefsl.trainer import Trainer
 from simplefsl.utils import seed_everything, get_model_loss, load_model
 
-def main(model_name: str, ways: int, shots: int, gpu: int, lr: float, l2_weight: float):
+def main(model_name: str, backbone: str, ways: int, shots: int, gpu: int, lr: float, l2_weight: float):
     seed = 42
-    backbone_name = 'resnet50.a3_in1k'
     episodes = 2
     epochs = 40
     validate_every = 1
+    
+    if backbone == "resnet":
+        backbone_name = 'resnet50.a3_in1k'
+    elif backbone == "swin":
+        backbone_name = 'swin_s3_tiny_224.ms_in1k'
+
 
     # setup
     seed_everything(seed)
@@ -54,8 +59,9 @@ if __name__ == "__main__":
     parser.add_argument('--shots', type=int, default=5, help='Number of examples per class (K-shot)')
     parser.add_argument('--gpu', type=int, default=0, help='GPU ID to use (default: 0)')
     parser.add_argument('--model', type=str, required=True, help='Model name')
+    parser.add_argument('--backbone', type=str, default="resnet", help='Backbone name')
     parser.add_argument('--lr', type=float, default=0.0001, help='Learning rate (default: 0.001)')
     parser.add_argument('--l2_weight', type=float, default=0.0, help='L2 regularization term (default: 0.0001)')
     
     args = parser.parse_args()
-    main(args.model, args.ways, args.shots, args.gpu, args.lr, args.l2_weight)
+    main(args.model, args.backbone, args.ways, args.shots, args.gpu, args.lr, args.l2_weight)
